fb874fe0ec8b6c2de8509fc53f59f68f36e34b95
Kirk DesRosiers <68282764+ktdesrosiers@users.noreply.github.com>
2025-07-08 13:27:37 -0400

    Update proj-x.js

--- proj-x.js
+++ proj-x.js
@@ -273,7 +273,8 @@ function loadquest(){
 // determine the number of questions so we can advance to results when all have been asked by setting a SL variable to hold the total num of questions.
 var q_count = player.GetVar("skill_ass_q_count");
 var cur_ass_code = player.GetVar("current_assessment");
-const numQuestions = Object.keys(ass_content).length;
+ const thisquiz = ass_content.filter((question) => question.includes(cur_ass_code));
+const numQuestions = Object.keys(thisquiz).length;
 player.SetVar("skill_ass_q_total",numQuestions);
 function filterQuestion(ass_content, questionKey) {
   if (ass_content.hasOwnProperty(questionKey)) {
