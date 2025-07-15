05719070c34af939e3e88650c6438c6f4012769a
Kirk DesRosiers <68282764+ktdesrosiers@users.noreply.github.com>
2025-07-15 10:54:43 -0400

    updated ass_content object.

--- proj-x.js
+++ proj-x.js
@@ -162,15 +162,15 @@ const l_data =[
  }
 ];
 const ass_content = {
-  "st1_q1": {
-    "stem": "You are an editor working at Dynamica Health Communications and have been given the opportunity to strategize a new publication about \"HY-P832,\" a drug aimed at treating chronic migraines. Your journey begins in the bustling office of Dynamica, where you're tasked with developing a comprehensive publication plan.\nIt's Monday morning, and you're in the conference room with your team. Your manager, Alex, outlines the importance of understanding the disease landscape. \nAlex asks, \"How do you plan to gather insights on chronic migraines?\"\nChoose a single response to Alex.",
+  "st_q1": {
+    "stem": "You are an editor working at Dynamica Health Communications and have been given the opportunity to strategize a new publication about \"HY-P832,\" a drug aimed at treating chronic migraines. Your journey begins in the bustling office of Dynamica, where you're tasked with developing a comprehensive publication plan.\nIt's Monday morning, and you're in the conference room with your team. Your manager, Alex, outlines the importance of understanding the disease landscape.\nAlex asks, \"How do you plan to gather insights on chronic migraines?\"\nChoose a single response to Alex.",
     "dist_1": "Use internet searches to gather basic information",
     "dist_2": "Review recent publications in medical journals",
     "dist_3": "Consult with industry experts and stakeholders",
     "dist_4": "Conduct a comprehensive literature review and interview leading key opinion leaders",
     "task_code": "st1"
   },
-  "st2_q1": {
+  "st_q2": {
     "stem": "Later, you're in the cafeteria grabbing lunch when your colleague Jamie joins you. Jamie is curious about what data you'll prioritize for the publication.\nJamie asks, \"What data are you thinking of highlighting?\"\nYou would be most likely to say you will...",
     "dist_1": "Emphasize safety data with minimal efficacy results",
     "dist_2": "Highlight efficacy results and include some patient testimonials",
@@ -178,7 +178,7 @@ const ass_content = {
     "dist_4": "Prioritize results of the primary endpoint of the trial and key secondary endpoints, including safety data",
     "task_code": "st2"
   },
-  "st3_q1": {
+  "st_q3": {
     "stem": "In the afternoon, you're in a brainstorming session with your team to assess potential audience needs as you plan further. Your task is to gather insights from healthcare professionals, patients, and regulatory bodies.\nYour team asks, \"How would we collect this information?\"\nYou would be most likely to respond with...",
     "dist_1": "Use past strategies because the strategies do not change much each year",
     "dist_2": "Conduct informal interviews with key stakeholders",
@@ -186,7 +186,7 @@ const ass_content = {
     "dist_4": "Conduct focus groups and comprehensive surveys with each audience",
     "task_code": "st3"
   },
-  "st4_q1": {
+  "st_q4": {
     "stem": "You step into your manager's office to discuss the scope of the publication. They need your final decision on the type of publication.\nAlex says, \"Which publication are you leaning towards?\"\nYou would be most likely to....",
     "dist_1": "Select a regulatory submission based on ease of completion",
     "dist_2": "Opt for a peer-reviewed journal article that aligns with commercial messaging",
@@ -194,7 +194,7 @@ const ass_content = {
     "dist_4": "Select a peer-reviewed journal article that aligns with clinical needs and audience information gaps",
     "task_code": "st4"
   },
-  "st5_q1": {
+  "st_q5": {
     "stem": "It's time to manage the publication planning committee. You're in a meeting room with representatives from various departments.\nA committee member asks, \"How will you gather input from the publication planning committee to ensure smooth execution of the publication plan?\"\nYour approach to this question would most likely be to...",
     "dist_1": "Schedule infrequent meetings with the clinical team",
     "dist_2": "Hold regular meetings with the clinical team for updates on data release",
@@ -202,7 +202,7 @@ const ass_content = {
     "dist_4": "Schedule regular meetings and ensure diverse representation from cross-functional teams",
     "task_code": "st5"
   },
-  "st6_q1": {
+  "st_q6": {
     "stem": "You are now ready to enter the details for the HY-P832 publication into your company’s scientific communications platform. This step is critical for ensuring the project is managed efficiently and in compliance with best practices. You must decide how thoroughly to complete the platform setup and documentation.\nAs you begin entering the publication project into the platform, which approach best describes how you would proceed?",
     "dist_1": "Enter only basic info and upload the draft. Use email for feedback. Need step-by-step guidance and frequent supervision.",
     "dist_2": "Complete all standard fields, upload drafts and documents, set up review workflow for key stakeholders. Work mostly independently, but need some guidance for compliance.",
@@ -210,7 +210,7 @@ const ass_content = {
     "dist_4": "Use all platform features—cross-link projects, customize approvals, automate compliance checks. Advise others, champion best practices, and innovate for audit readiness.",
     "task_code": "st6"
   },
-  "st7_q1": {
+  "st_q7": {
     "stem": "In a quiet corner of the office, you're discussing publication strategy with your mentor, Taylor. The urgency to publish data on HY-P832 is high, as physicians need timely information on long-term safety.\nTaylor asks, \"Given the need for rapid and credible communication, what is the best approach to develop the publication and its plain language summary (PLS)?\"\nYou would be most likely to…",
     "dist_1": "Add the PLS as a scheduled step after the manuscript, but not integrated into the main workflow.",
     "dist_2": "Make the PLS a separate deliverable with its own timeline and budget, developed independently.",
@@ -218,7 +218,7 @@ const ass_content = {
     "dist_4": "Fully integrate the PLS: develop it concurrently, involve patient advocates, and target journals that peer-review and index PLSs",
     "task_code": "st7"
   },
-  "st8_q1": {
+  "st_q8": {
     "stem": "As the day winds down, you're at your desk developing post-publication timelines.\nYour colleague, Sam, stops by, \"Not sure if you saw the company wide memo but they want us to make sure to include plain language summaries in all future primary and secondary publications. I guess your new project falls into this category. How are you going to plan for it?”\nHow are you most likely to approach the new requirement?",
     "dist_1": "Adjust plans occasionally based on a quick review of plain language summary methods.",
     "dist_2": "Draw on your existing knowledge to define a plan given you regularly review industry trends.",
@@ -226,7 +226,7 @@ const ass_content = {
     "dist_4": "Happily implement the guidelines and standards you have developed as lead of a internal and external working group related to developing plain language summaries.",
     "task_code": "st8"
   },
-  "im1_q1": {
+  "im_q1": {
     "stem": "You are a Publications Manager at Innovate Pharma, and the publication plan for HY-P832 is approved. Your next step is to begin implementing this plan, starting with identifying authors for a key peer-reviewed manuscript.\nYou need to identify a lead author for the primary publication reporting the Phase III clinical trial results of HY-P832.\nWhich of the following would be your first step in identifying a lead author?",
     "dist_1": "Immediately reach out to the most senior physician listed on the study protocol.",
     "dist_2": "Send an email to all investigators involved in the Phase III trial asking for volunteers.",
@@ -234,7 +234,7 @@ const ass_content = {
     "dist_4": "Consult with your internal medical writing team to see if they can take on the lead authorship role.",
     "task_code": "im1"
   },
-  "im2_q1": {
+  "im_q2": {
     "stem": "The draft manuscript for the HY-P832 Phase III trial is nearing completion. You need to manage the review and approval process efficiently.\nWhat would you emphasize when initiating the review process with the authors and relevant stakeholders?",
     "dist_1": "The importance of a quick turnaround to meet the target submission date.",
     "dist_2": "Focusing solely on grammatical errors and formatting issues.",
@@ -242,7 +242,7 @@ const ass_content = {
     "dist_4": "That only minor edits are expected at this stage to avoid delays.",
     "task_code": "im2"
   },
-  "im3_q1": {
+  "im_q3": {
     "stem": "You are juggling multiple publications within the HY-P832 plan. Keeping track of progress and deadlines is becoming challenging.\nWhat approach would you choose to manage the administrative aspects of your publication plan and ensure all deliverables are on track?",
     "dist_1": "Rely on email reminders and personal notes.",
     "dist_2": "Schedule ad-hoc meetings to discuss progress on each publication.",
@@ -250,7 +250,7 @@ const ass_content = {
     "dist_4": "Delegate all administrative tasks to an administrative assistant.",
     "task_code": "im3"
   },
-  "im4_q1": {
+  "im_q4": {
     "stem": "The first few publications from the HY-P832 plan have been published. Your manager, Sarah, wants to understand the impact of these publications.\nSarah asks, \"How are we tracking the success of these initial HY-P832 publications?\"\nYou would be most likely to respond that you are...",
     "dist_1": "Monitoring the number of downloads from the journal websites.",
     "dist_2": "Tracking the number of citations the publications receive.",
@@ -258,7 +258,7 @@ const ass_content = {
     "dist_4": "Tracking a range of metrics, including journal impact factor, citation counts, website/social media engagement, and alignment with the overall communication objectives.",
     "task_code": "im4"
   },
-  "im5_q1": {
+  "im_q5": {
     "stem": "Months into the implementation of the HY-P832 publication plan, you notice a growing trend in the field towards publishing open-access articles.\nHow would you respond to this evolving trend in the context of your ongoing publication plan?",
     "dist_1": "Ignore the trend as the current plan is already established.",
     "dist_2": "Immediately switch all planned publications to open access, regardless of budget or journal suitability.",
@@ -266,7 +266,7 @@ const ass_content = {
     "dist_4": "Discuss the trend with the legal department to understand any potential compliance issues.",
     "task_code": "im5"
   },
-  "et1_q1": {
+  "et_q1": {
     "stem": "You are drafting a publication outlining a post-hoc analysis of the HY-P832 clinical trial data. A new guideline from a relevant professional society regarding post-hoc analyses has recently been released.\nWhat would you current approach be to reviewing the new guideline?",
     "dist_1": "Assume the existing publication plan is sufficient and proceed without reviewing the new guideline.",
     "dist_2": "Quickly skim the guideline to see if it mentions HY-P832 specifically.",
@@ -274,7 +274,7 @@ const ass_content = {
     "dist_4": "Delegate the responsibility of reviewing the guideline to a junior team member.",
     "task_code": "et1"
   },
-  "et2_q1": {
+  "et_q2": {
     "stem": "During the author identification process for a HY-P832 publication, a senior investigator who made minimal contributions to the study insists on being listed as a co-author due to their seniority.\nHow would you elect to address this situation?",
     "dist_1": "Agree to include the investigator to maintain good relationships.",
     "dist_2": "Politely explain the authorship criteria (e.g., ICMJE recommendations) and engage in a transparent discussion about their contributions.",
@@ -282,7 +282,7 @@ const ass_content = {
     "dist_4": "Escalate the issue to your manager without attempting to resolve it directly.",
     "task_code": "et2"
   },
-  "et3_q1": {
+  "et_q3": {
     "stem": "You are collecting disclosure information from the authors of a HY-P832 review article. One of the authors discloses a significant financial interest in a competing product.\nHow would you handle this disclosure in the publication?",
     "dist_1": "Omit the disclosure to avoid potential bias concerns.",
     "dist_2": "Simply list the disclosure without providing any context.",
@@ -290,7 +290,7 @@ const ass_content = {
     "dist_4": "Ask the author to withdraw from the publication.",
     "task_code": "et3"
   },
-  "et4_q1": {
+  "et_q4": {
     "stem": "Several individuals, including medical writers and statisticians, have made significant contributions to the development of a HY-P832 manuscript but do not meet the criteria for authorship.\nHow would you approach recognition in this situation?",
     "dist_1": "No acknowledgement is necessary as they are paid for their services.",
     "dist_2": "Include a general statement thanking \"all contributors.\"",
@@ -298,7 +298,7 @@ const ass_content = {
     "dist_4": "Only acknowledge individuals from within your own company.",
     "task_code": "et4"
   },
-  "et5_q1": {
+  "et_q5": {
     "stem": "You attend a conference and learn about increasing scrutiny from regulatory bodies regarding the transparency of industry-sponsored publications and the use of plain language summaries.\nHow would you incorporate this evolving trend into your ongoing and future publication plans for HY-P832?",
     "dist_1": "Wait for formal guidance from your company's legal department before making any changes.",
     "dist_2": "Continue with the current processes as they have been previously approved.",
@@ -307,6 +307,7 @@ const ass_content = {
     "task_code": "et5"
   }
 };
+
 
 // this helper function accounts for poor text handling abilities in storyline and replacees newline with <br><br>
 function replaceNewlines(text) {
