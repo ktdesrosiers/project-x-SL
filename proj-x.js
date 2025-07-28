7c4e5e5fca81e6276fa0112aafdb449eeab8efc8
Kirk DesRosier <ktdesrosier@gmail.com>
2025-07-28 15:36:43 -0400

    edits

--- proj-x.js
+++ proj-x.js
@@ -1777,6 +1777,8 @@ function launchlesson(code) {
   quizWindow = window.open(lesson_url, '_blank');
   lastopened_lesson = origCode;
   player.SetVar(origCode + "_cur_score",999);
+  updateDomainScore(domain);
+  displaycoaching_progress(domain)
   orderDomainCards(domain);
   coach(domain,domain+"_coach_message","CH");
   processHighlight();
